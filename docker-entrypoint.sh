471dc58a9fe7889de0c0b56c10b7bc8913e21f0c
realwrtoff <fmj_912@163.com>
2019-06-03 20:07:03 +0800

    add replication user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -187,6 +187,13 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
 
+		file_env 'MYSQL_REPL_USER'
+		file_env 'MYSQL_REPL_PASSWORD'
+		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+		    echo "GRANT REPLICATION CLIENT,REPLICATION SLAVE ON . TO '$MYSQL_REPL_USER'@'%' IDENTIFIED BY '$MYSQL_REPL_PASSWORD';"| "${mysql[@]}"
+		    echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+		fi
+
 		echo
 		ls /docker-entrypoint-initdb.d/ > /dev/null
 		for f in /docker-entrypoint-initdb.d/*; do
